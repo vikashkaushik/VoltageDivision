bdcab8da6f7fc3cbabf3494864d12c3a61182123
vikashkaushik <vikakau@gmail.com>
2021-07-02 10:45:31 -0500

    final callebration

--- src/VoltageDivision.ino
+++ src/VoltageDivision.ino
@@ -7,16 +7,14 @@
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 uint16_t value; // initallizes a max a nd min value for calabration;
-uint16_t maxValue; 
-uint16_t minValue; 
+uint16_t maxValue = 0; 
+uint16_t minValue = 4095; 
 // setup() runs once, when the device is first turned on.
 void setup() {
   // Put initialization like pinMode and begin functions here.
   pinMode(D6, OUTPUT);
   pinMode(A5, INPUT);
-
-  maxValue = max(maxValue, value); //sets a max value
-  minValue = min(minValue , value); // sets a min value
+  Serial.begin(9600); 
   
 }
 
@@ -25,11 +23,12 @@ void loop() {
   // The core of your code will likely live here.
   
  
-  Serial.begin(9600); 
-  value = analogRead(A5); // reads the value that the LUX has
   
+  value = analogRead(A5); // reads the value that the LUX has
+  maxValue = max(maxValue, value); //sets a max value
+  minValue = min(minValue , value); // sets a min value
   Serial.println(value); 
- 
+  
   analogWrite(D6, map(value, minValue, maxValue, 0, 255)); // scale value to the min and max value range
   
 }
